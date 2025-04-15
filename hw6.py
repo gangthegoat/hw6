d24bfe3505a985c8bf5812ee715672c7c58a53ba
gangthegoat <bini15389@gmail.com>
2025-04-15 20:53:07 +0900

    Update hw6.py

--- hw6.py
+++ hw6.py
@@ -1,8 +1,8 @@
 def print_gugudan(start, end):
-    for i in range(1, 10):  # 각 단의 곱 (1~9)
-        for dan in range(start, end + 1):  # 단의 범위 (2~5, 6~9)
+    for i in range(1, 10): 
+        for dan in range(start, end + 1):  
             print(f"{dan} x {i} = {dan*i:2}", end="\t")
-        print()  # 줄 바꿈
+        print()  
 
 print_gugudan(2, 5)
 
